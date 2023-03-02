fea5fbfc1f4b62bde4b2317d45437b4e3c2f2f8a
Gustavo Souza <GustavoSenai500@gmail.com>
2023-03-02 16:54:30 -0300

    contatos

--- ds2t/gustavo_souza_tenorio_de_barros/index.js
+++ ds2t/gustavo_souza_tenorio_de_barros/index.js
@@ -1,2 +1,31 @@
 'use strict'
 
+import{contatos} from "./contatos.js"
+
+
+const criaCard = (contatos) =>{
+    const card = document.createElement("div")
+    card.classList.add ('card')
+
+    const foto = document.createElement('img')
+    foto.classList.add('card_image')
+    foto.src = contatos.image
+
+    const nome = document.createElement('h5')
+    nome.classList.add('card_nome')
+    nome.textContent = contatos.nome
+
+    const descricao = document.createElement('a')
+    descricao.classList.add('card_description')
+    descricao.textContent = contatos.descricao
+
+    card.append(foto, nome , descricao)
+    return card
+}
+const carregarContatos = ()=> {
+    const container = document.getElementById('container_contatos')
+    const cards = contatos.map(criaCard)
+
+    container.replaceChildren(...cards)
+}
+carregarContatos()
